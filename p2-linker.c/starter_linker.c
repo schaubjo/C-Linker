97ad7046093296252ac68c72f07b420b616f309d
JoquijoteJoflamingo <schaubjo@umich.edu>
2023-02-27 11:06:15 -0500

    test

---
+++ p2-linker.c/starter_linker.c
@@ -0,0 +1,210 @@
+/**
+ * Project 2
+ * LC-2K Linker
+ */
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXSIZE 300
+#define MAXLINELENGTH 1000
+#define MAXFILES 6
+
+typedef struct FileData FileData;
+typedef struct SymbolTableEntry SymbolTableEntry;
+typedef struct RelocationTableEntry RelocationTableEntry;
+typedef struct CombinedFiles CombinedFiles;
+
+int resolve_local();
+struct SymbolTableEntry {
+	char label[7];
+	char location;
+	int offset;
+};
+
+struct RelocationTableEntry {
+	int offset;
+	char inst[7];
+	char label[7];
+	int file;
+};
+
+struct FileData {
+	int textSize;
+	int dataSize;
+	int symbolTableSize;
+	int relocationTableSize;
+	int textStartingLine; // in final executable
+	int dataStartingLine; // in final executable
+	int text[MAXSIZE];
+	int data[MAXSIZE];
+	SymbolTableEntry symbolTable[MAXSIZE];
+	RelocationTableEntry relocTable[MAXSIZE];
+};
+
+struct CombinedFiles {
+	int text[MAXSIZE];
+	int data[MAXSIZE];
+	SymbolTableEntry     symTable[MAXSIZE];
+	RelocationTableEntry relocTable[MAXSIZE];
+	int textSize;
+	int dataSize;
+	int symTableSize;
+	int relocTableSize;
+};
+
+int main(int argc, char *argv[])
+{
+	char *inFileString, *outFileString;
+	FILE *inFilePtr, *outFilePtr; 
+	int i, j;
+
+	if (argc <= 2) {
+		printf("error: usage: %s <obj file> ... <output-exe-file>\n",
+				argv[0]);
+		exit(1);
+	}
+
+	outFileString = argv[argc - 1];
+
+	outFilePtr = fopen(outFileString, "w");
+	if (outFilePtr == NULL) {
+		printf("error in opening %s\n", outFileString);
+		exit(1);
+	}
+
+	FileData files[MAXFILES];
+
+  // read in all files and combine into a "master" file
+	for (i = 0; i < argc - 2; i++) {
+		inFileString = argv[i+1];
+
+		inFilePtr = fopen(inFileString, "r");
+		printf("opening %s\n", inFileString);
+
+		if (inFilePtr == NULL) {
+			printf("error in opening %s\n", inFileString);
+			exit(1);
+		}
+
+		char line[MAXLINELENGTH];
+		int sizeText, sizeData, sizeSymbol, sizeReloc;
+
+		// parse first line of file
+		fgets(line, MAXSIZE, inFilePtr);
+		sscanf(line, "%d %d %d %d",
+				&sizeText, &sizeData, &sizeSymbol, &sizeReloc);
+
+		files[i].textSize = sizeText;
+		files[i].dataSize = sizeData;
+		files[i].symbolTableSize = sizeSymbol;
+		files[i].relocationTableSize = sizeReloc;
+
+		// read in text section
+		int instr;
+		for (j = 0; j < sizeText; j++) {
+			fgets(line, MAXLINELENGTH, inFilePtr);
+			instr = atoi(line);
+			files[i].text[j] = instr;
+		}
+
+		// read in data section
+		int data;
+		for (j = 0; j < sizeData; j++) {
+			fgets(line, MAXLINELENGTH, inFilePtr);
+			data = atoi(line);
+			files[i].data[j] = data;
+		}
+
+		// read in the symbol table
+		char label[7];
+		char type;
+		int addr;
+		for (j = 0; j < sizeSymbol; j++) {
+			fgets(line, MAXLINELENGTH, inFilePtr);
+			sscanf(line, "%s %c %d",
+					label, &type, &addr);
+			files[i].symbolTable[j].offset = addr;
+			strcpy(files[i].symbolTable[j].label, label);
+			files[i].symbolTable[j].location = type;
+		}
+
+		// read in relocation table
+		char opcode[7];
+		for (j = 0; j < sizeReloc; j++) {
+			fgets(line, MAXLINELENGTH, inFilePtr);
+			sscanf(line, "%d %s %s",
+					&addr, opcode, label);
+			files[i].relocTable[j].offset = addr;
+			strcpy(files[i].relocTable[j].inst, opcode);
+			strcpy(files[i].relocTable[j].label, label);
+			files[i].relocTable[j].file	= i;
+		}
+		fclose(inFilePtr);
+	} // end reading files
+
+	// *** INSERT YOUR CODE BELOW ***
+	//    Begin the linking process
+	//    Happy coding!!!
+    
+    // iterate through all files
+    
+    for (int f = 0; f < argc - 2; f++) {
+        
+        // iterate through each line in text section
+        for (int t = 0; t < files[f].textSize; t++) {
+            bool resolve = false;
+            
+            // check the relocation table to see if it is a line that needs to be resolved
+            for (int r = 0; r < files[f].relocationTableSize; r++) {
+                if (files[f].relocTable[r].offset == t) {
+                    resolve = true;
+                    break;
+                }
+            }
+            
+            // if this line was in the relocation table, we need to resolve the offset
+            if (resolve) {
+                
+                // local labels
+                if (!isupper(files[f].relocTable[t].label[0])) {
+                    // add text lines from files before this file to offset
+                    int before = 0;
+                    for (int b = 0; b < t; b++) {
+                        before += files[b].textSize;
+                    }
+                    
+                    // add text lines from files after this file to offset
+                    int after = 0;
+                    for (int a = f + 1; a < argc - 2; a++) {
+                        after += files[a].textSize;
+                    }
+                    
+                    fprintf(outFilePtr, "%d\n", files[f].text[t] + before + after);
+                }
+                
+                // global labels
+                else {
+                    
+                }
+                
+            }
+            else {
+                fprintf(outFilePtr, "%d\n", files[f].text[t]);
+            }
+        }
+    }
+
+} // main
+
+int resolve_loca() {
+    
+    // resolve local labels that have to point to data section within its own file
+    int before = 0;
+//    for (int b = 0; b < t; b++) {
+//
+//    }
+    return 0;
+}
